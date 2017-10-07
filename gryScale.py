ced450f7f9bef619a33413c49d60d06d7c1a137b
Ihapmustapha <ihapmustapha@ymail.com>
2017-10-07 19:47:52 +0200

    more organized code

--- gryScale.py
+++ gryScale.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import cv2
 
 ########-Testing Gray Scale Functionality-##########
@@ -21,28 +21,4 @@ cv2.destroyAllWindows()
 img = cv2.imread('./images/dog.jpg',0)
 cv2.imshow('GrayScale II', img)
 cv2.waitKey()
-=======
-import cv2
-
-########-Testing Gray Scale Functionality-##########
-
-#load input image
-input = cv2.imread('.\images\dog.jpg')
-cv2.imshow('Original',input)
-cv2.waitKey()
-
-#use cvtColor to convert to grayScale
-gray_img = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
-
-#show output image after converting
-cv2.imshow('GrayScale Image', gray_img)
-cv2.waitKey()
-cv2.destroyAllWindows()
-
-#Another Faster Method is to get gray scale image
-#the idea is when you put '0' as a second argument
-img = cv2.imread('./images/dog.jpg',0)
-cv2.imshow('GrayScale II', img)
-cv2.waitKey()
->>>>>>> master
 cv2.destroyAllWindows()
